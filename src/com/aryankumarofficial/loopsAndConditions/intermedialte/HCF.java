25738b1711d7856930c5d7288e03b5a83d4ddc0f
Aryan Kumar <aryanak9163@gmail.com>
2025-08-12 23:13:16 +0530

    HCF

---
+++ src/com/aryankumarofficial/loopsAndConditions/intermedialte/HCF.java
@@ -0,0 +1,26 @@
+package com.aryankumarofficial.loopsAndConditions.intermedialte;
+
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the first number for HFC: ");
+            int firstNumber = sc.nextInt();
+            System.out.print("Enter the second number for HFC: ");
+            int secondNumber = sc.nextInt();
+
+            int hcf = calculateHCF(firstNumber, secondNumber);
+            System.out.println("HCF of 10 and 20 is: " + hcf);
+        }
+    }
+
+    private static int calculateHCF(int firstNumber, int secondNumber) {
+        while (secondNumber != 0) {
+            int r = firstNumber % secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = r;
+        }
+        return firstNumber;
+    }
+}
