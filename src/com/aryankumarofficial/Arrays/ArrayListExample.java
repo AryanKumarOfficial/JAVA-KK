2d0192b93624393f185c1cc8f7d71ab6744fcc37
Aryan Kumar <aryanak9163@gmail.com>
2025-08-16 16:26:09 +0530

    completed ArrayList

---
+++ src/com/aryankumarofficial/Arrays/ArrayListExample.java
@@ -0,0 +1,31 @@
+package com.aryankumarofficial.Arrays;
+
+import java.util.*;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(10);
+//        list.add(6);
+//        list.add(74);
+//        list.add(55);
+//
+//        System.out.println(list);
+//        list.set(0, 99);
+//        list.remove(0);
+
+//        for (int i = 0; i < 5; i++) {
+//            list.add(in.nextInt());
+//        }
+
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.print(list.get(i) + " ");
+//        }
+//
+//        System.out.println(list);
+//        System.out.println(list.getFirst());
+//        System.out.println(list.getLast());
+
+    }
+
+}
